0308d4effa5a10436e6d4aa5410a1f103a8a08a8
GMAIL <EMAIL>
2021-11-18 09:18:24 +0700

    edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <v-app>
-    <v-container>
-      <Nuxt />
-    </v-container>
+    <div class="bg darken-2 text-center">
+      <span class="white--text">
+        <v-container>
+          <Nuxt />
+        </v-container>
+      </span>
+    </div>
   </v-app>
 </template>
+
+<style>
+.bg{
+    background-color: #FF1744;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,57 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="pa-7 white rounded-circle d-inline-block">
+          <v-icon large color="blue">
+            mdi-cloud-upload
+          </v-icon>
+        </div>
+      </v-col>
+    </v-row>
+    <h1 style="margin-top:20px;">
+      GF BANK
+    </h1>
+    <h3>MOBILE APP</h3>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-text-field
+              label="PLEATE ENTER YOUR PASSWORD"
+              placeholder="PASSWORD"
+              filled
+              rounded
+              dense
+            />
+            <v-btn class="btn1">
+              <samp class="txt1">LOGIN</samp>
+            </v-btn>
+            <p>Forgot password?</p>
+            <p>New to Bank App? Sign Up</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
+
+<style>
+.pa-7{
+  margin-top: 100px;
+}
+h3{
+  margin-bottom: 50px;
+}
+.txt1{
+  color: brown;
+}
+.btn1{
+  background: linear-gradient(to bottom,#FFEB3B,#FFC107);
+  margin-bottom: 50px;
+}
+</style>
